612fa999e44f206eb0996d72d8d403f769c31443
Vaishnavi <vj222@njit.edu>
2018-11-12 21:59:24 -0500

    Added Question migrate,model,factory and created tests for questions

--- app/User.php
+++ app/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable
         return $this->hasOne('App\Profile');
     }
 
+    public function questions()
+    {
+        return $this->hasMany('App\Question');
+    }
+
 }
---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+
+
+    public function testSave()
+    {
+        $user = factory(\App\User::class)->make();
+        $this->assertTrue($user->save());
+    }
+
+    public function testQuestions()
+    {
+        $user = factory(\App\User::class)->make();
+        $this->assertTrue(is_object($user->questions()->get()));
+    }
+
+}
