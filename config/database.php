72faf61371efd2c6a16ce04ec73dea6ae3d4029c
Vaishnavi <vj222@njit.edu>
2018-11-13 19:28:20 -0500

    Created User Table Seeder

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-
+$heroku_db_url = parse_url(env('DATABASE_URL', "postgres://forge:forge@localhost:5432/forge"));
 return [
 
     /*
@@ -81,7 +81,17 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
         ],
-
+        
+        'pg-heroku' => [
+            'driver' => 'pgsql',
+            'host' => $heroku_db_url['host'],
+            'database' => substr($heroku_db_url['path'], 1),
+            'username' => $heroku_db_url['user'],
+            'password' => $heroku_db_url['pass'],
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+        ],
     ],
 
     /*
