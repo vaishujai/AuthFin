3e7096f5814d432b20908ce4f7511fef35c43b68
Vaishnavi <vj222@njit.edu>
2018-11-20 11:56:32 -0500

    Added user profile details

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/user/{user_id}/profile/{profile_id}', 'ProfileController@show')->name('profile.show');
