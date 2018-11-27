98a452c062e1809af751e79262e4068c15859945
Vaishnavi <vj222@njit.edu>
2018-11-27 04:37:14 -0500

    Feature : Created,updated,edited and delete Answerform

--- app/Http/Controllers/AnswerController.php
+++ app/Http/Controllers/AnswerController.php
@@ -1,47 +1,53 @@
 <?php
-
 namespace App\Http\Controllers;
 use App\Answer;
+use App\Question;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
-
 class AnswerController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     *
-     */
     public function __construct()
     {
         $this->middleware('auth');
     }
-    public function index()
-    {
-        //
-    }
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     /**
      * Show the form for creating a new resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
+    public function create($question)
     {
-        //
+        $answer = new Answer;
+        $edit = FALSE;
+        return view('answerForm', ['answer' => $answer,'edit' => $edit, 'question' =>$question  ]);
     }
-
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $request, $question)
     {
-        //
+        $input = $request->validate([
+            'body' => 'required|min:5',
+        ], [
+            'body.required' => 'Body is required',
+            'body.min' => 'Body must be at least 5 characters',
+        ]);
+        $input = request()->all();
+        $question = Question::find($question);
+        $Answer = new Answer($input);
+        $Answer->user()->associate(Auth::user());
+        $Answer->question()->associate($question);
+        $Answer->save();
+        return redirect()->route('questions.show',['question_id' => $question->id])->with('message', 'Saved');
     }
-
     /**
      * Display the specified resource.
      *
@@ -53,18 +59,18 @@ class AnswerController extends Controller
         $answer = Answer::find($answer);
         return view('answer')->with(['answer' => $answer, 'question' => $question]);
     }
-
     /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit($question,  $answer)
     {
-        //
+        $answer = Answer::find($answer);
+        $edit = TRUE;
+        return view('answerForm', ['answer' => $answer, 'edit' => $edit, 'question'=>$question ]);
     }
-
     /**
      * Update the specified resource in storage.
      *
@@ -72,19 +78,30 @@ class AnswerController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, $question, $answer)
     {
-        //
-    }
+        $input = $request->validate([
+            'body' => 'required|min:5',
+        ], [
+            'body.required' => 'Body is required',
+            'body.min' => 'Body must be at least 5 characters',
+        ]);
 
+        $answer = Answer::find($answer);
+        $answer->body = $request->body;
+        $answer->save();
+        return redirect()->route('answers.show',['question_id' => $question, 'answer_id' => $answer])->with('message', 'Updated');
+    }
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy($question, $answer)
     {
-        //
+        $answer = Answer::find($answer);
+        $answer->delete();
+        return redirect()->route('questions.show',['question_id' => $question])->with('message', 'Delete');
     }
 }
--- resources/views/question.blade.php
+++ resources/views/question.blade.php
@@ -24,7 +24,6 @@
                     </div>
                 </div>
             </div>
-
             <div class="col-md-4">
                 <div class="card">
                     <div class="card-header"><a class="btn btn-primary float-left"
